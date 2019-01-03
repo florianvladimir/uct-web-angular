d16266b337e61e7fd01ab7900c3c9de47feda4e6
Florian <florian98.moser@outlook.de>
2019-01-03 15:24:07 +0100

    animatione

--- src/app/custom-components/information-card/information-card.component.ts
+++ src/app/custom-components/information-card/information-card.component.ts
@@ -1,13 +1,32 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, HostListener, Input, OnInit, Output} from '@angular/core';
 import {DataInformationCard, TextInformationCard} from './model/data-information-card.interface';
-import {e} from '@angular/core/src/render3';
+import {animate, state, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'information-card',
   templateUrl: './information-card.component.html',
-  styleUrls: ['./information-card.component.css']
+  styleUrls: ['./information-card.component.css'],
+  animations: [
+    trigger('scrollAnimation', [
+      state('show', style({
+        opacity: 1,
+        // transform: 'translateY(0) scale(1)'
+        transform: 'scale(1.0)'
+      })),
+      state('hide',   style({
+        opacity: 0,
+        // transform: 'translateX(-100%) scale(0.8)'
+        // transform: 'translateY(100%) scale(0.2)',
+        transform: 'scale(0)'
+      })),
+      transition('show => hide', animate('1s ease-out')),
+      transition('hide => show', animate('1s ease-in')),
+    ])
+  ]
 })
-export class InformationCardComponent {
+export class InformationCardComponent implements OnInit{
+
+  state= 'hide';
 
   @Input()
   public information: DataInformationCard;
@@ -22,7 +41,23 @@ export class InformationCardComponent {
   @Output()
   scroll = new EventEmitter<Element>();
 
-  constructor() { }
+  constructor(public element: ElementRef) { }
+
+  ngOnInit(): void {
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  checkScroll() {
+    const componentPosition = this.element.nativeElement.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (componentPosition >= windowHeight - (windowHeight*0.1)) {
+      this.state = 'hide';
+    } else {
+      this.state = 'show';
+    }
+
+  }
 
   scrollTo(link) {
     if (link === 'btn1') {
